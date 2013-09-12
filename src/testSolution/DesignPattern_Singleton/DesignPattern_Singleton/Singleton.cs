b45cff2bc421c2e3d9aa0d5512bdecde22cb1f7c
cjjack88 <jie26488@hotmail.com>
2013-09-12 17:02:48 +0800

    Singleton pattern

---
+++ src/testSolution/DesignPattern_Singleton/DesignPattern_Singleton/Singleton.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesignPattern_Singleton
+{
+    public class Singleton
+    {
+        // .NET guarantees thread safety for static initialization
+        private static Singleton instance = null;
+        private string Name { get; set; }
+        private string IP { get; set; }
+
+        private Singleton()
+        {
+            Name = "Server1";
+            IP = "192.168.1.23";
+        }
+
+        // Lock synchronization object
+        private static object syncLock = new object();
+
+        public static Singleton Instance
+        {
+            get
+            {
+                // Support multithreaded applications through
+                // 'Double checked locking' pattern which (once
+                // the instance exists) avoids locking each
+                // time the method is invoked
+                lock (syncLock)
+                {
+                    if (Singleton.instance == null)
+                        Singleton.instance = new Singleton();
+
+                    return Singleton.instance;
+                }
+            }
+        }
+
+        public string Show()
+        {
+            return string.Format("Server Information is : Name={0} & IP={1}", IP, Name);
+        }
+    }
+}
---
+++ src/testSolution/DesignPattern_Test/Singleton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DesignPattern_Test
+{
+    [TestClass]
+    public class Singleton
+    {
+        [TestMethod]
+        public void TestSingleton()
+        {
+            Assert.IsNotNull(DesignPattern_Singleton.Singleton.Instance.Show());
+        }
+    }
+}
