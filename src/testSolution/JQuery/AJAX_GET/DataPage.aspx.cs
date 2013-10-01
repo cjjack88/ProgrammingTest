7854ce25d078196e4e92c29d8a7cf0dcbca696b9
Chan Chen Jie <jie26488@hotmail.com>
2013-10-02 00:03:30 +0800

    Added Project AJAX_GET

---
+++ src/testSolution/JQuery/AJAX_GET/DataPage.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class DataPage : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string MemberName = Request.QueryString["MemberName"];
+
+        Response.Clear();
+        Response.ClearContent();
+        Response.ClearHeaders();
+        Response.ContentType = "text/plain";
+        Response.Write("Hello! " + MemberName);
+    }
+}
